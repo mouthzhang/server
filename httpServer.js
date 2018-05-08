b3931ed60ee25bb24ec2e7d8c869508c4b7ca93f
bowen zhang <ucsebz0@ucl.ac.uk>
2018-05-08 15:46:40 -0400

    maybe final ver.

--- httpServer.js
+++ httpServer.js
@@ -159,4 +159,47 @@ var app = express();
 	 
 	 app.get('/',function (req,res) {
 			res.send("hello world from the HTTP server");
-		}); 
+		});
+ 
+
+	// the / indicates the path that you type into the server - in this case, what happens when you type in:  http://developer.cege.ucl.ac.uk:32560/xxxxx/xxxxx
+  app.get('/:name1', function (req, res) {
+  // run some server-side code
+  // the console is the command line of your server - you will see the console.log values in the terminal window
+  console.log('request '+req.params.name1);
+
+  // the res is the response that the server sends back to the browser - you will see this text in your browser window
+  res.sendFile(webcontentpath + '/'+req.params.name1);
+});
+
+
+  // the / indicates the path that you type into the server - in this case, what happens when you type in:  http://developer.cege.ucl.ac.uk:32560/xxxxx/xxxxx
+  app.get('/:name1/:name2', function (req, res) {
+  // run some server-side code
+  // the console is the command line of your server - you will see the console.log values in the terminal window
+  console.log('request '+req.params.name1+"/"+req.params.name2);
+
+  // the res is the response that the server sends back to the browser - you will see this text in your browser window
+  res.sendFile(webcontentpath + '/'+req.params.name1+'/'+req.params.name2);
+});
+
+
+	// the / indicates the path that you type into the server - in this case, what happens when you type in:  http://developer.cege.ucl.ac.uk:32560/xxxxx/xxxxx/xxxx
+	app.get('/:name1/:name2/:name3', function (req, res) {
+		// run some server-side code
+		// the console is the command line of your server - you will see the console.log values in the terminal window
+		console.log('request '+req.params.name1+"/"+req.params.name2+"/"+req.params.name3); 
+		// send the response
+		res.sendFile(webcontentpath + '/'+req.params.name1+'/'+req.params.name2+ '/'+req.params.name3);
+	});
+  // the / indicates the path that you type into the server - in this case, what happens when you type in:  http://developer.cege.ucl.ac.uk:32560/xxxxx/xxxxx/xxxx
+  app.get('/:name1/:name2/:name3/:name4', function (req, res) {
+  // run some server-side code
+  // the console is the command line of your server - you will see the console.log values in the terminal window
+ console.log('request '+req.params.name1+"/"+req.params.name2+"/"+req.params.name3+"/"+req.params.name4); 
+  // send the response
+  res.sendFile(webcontentpath + '/'+req.params.name1+'/'+req.params.name2+ '/'+req.params.name3+"/"+req.params.name4);
+});
+  
+ 
+
